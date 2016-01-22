a1b16d16e3e57eebfcb9be68d6a405283cb5c2d2
bklo94 <brandonklo94@gmail.com>
2016-01-21 16:46:50 -0800

    Fixed nagivation bar

---
+++ Website/Current/content/header.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Current Pi - Raspberry Pi Status</title>
+
+<link href="favicon.ico" type="image/x-icon" rel="icon" />
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="./css/fontello.css">
+<!-- Custom Fonts -->
+<link href="./css/fonts.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="./css/custom.css" rel="stylesheet">
