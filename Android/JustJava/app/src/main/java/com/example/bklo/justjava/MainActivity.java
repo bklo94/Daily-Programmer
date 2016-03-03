cc349e31f9cb2b3ec9784c0d3ab25f28ea0a54c9
Brandon Lo <brandonklo94@gmail.com>
2016-03-02 23:13:26 -0800

    first commit

---
+++ Android/JustJava/app/src/main/java/com/example/bklo/justjava/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.bklo.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        String priceMessage = "Amount Due: $" + (quantity*5) + "\n" + "Thank you!";
+        displayMessage(priceMessage);
+    }
+
+    public void increment (View view){
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void deincrement (View view){
+        if (quantity > 0)
+            quantity = quantity - 1;
+        display(quantity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void displayMessage(String message){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+}
