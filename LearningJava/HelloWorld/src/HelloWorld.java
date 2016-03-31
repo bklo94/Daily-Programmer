dc8cfb6e819b839081f2ee1a2c6af21a72c8a945
Brandon Lo <brandonklo94@gmail.com>
2016-03-30 18:48:40 -0700

    first commit

---
+++ LearningJava/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bklo on 3/29/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ LearningJava/HelloWorld/src/JavaInput.java
@@ -0,0 +1,7 @@
+/**
+ * Created by bklo on 3/29/16.
+ */
+import java.util.scanner;
+
+public class JavaInput {
+}
